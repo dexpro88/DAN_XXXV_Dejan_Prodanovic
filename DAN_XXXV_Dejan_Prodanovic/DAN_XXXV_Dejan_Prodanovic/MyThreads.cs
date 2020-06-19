63fa8aeb30992a66313e7ed4a600ca4c3d362856
dexpro988 <dejan.prodanovic@panonit.com>
2020-06-19 03:08:33 -0700

    Added idea for method that guess wanted number

--- DAN_XXXV_Dejan_Prodanovic/DAN_XXXV_Dejan_Prodanovic/MyThreads.cs
+++ DAN_XXXV_Dejan_Prodanovic/DAN_XXXV_Dejan_Prodanovic/MyThreads.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAN_XXXV_Dejan_Prodanovic
 {
     class MyThreads
     {
+        private int numberOfParticipants;
+        private int wantedNumber;
+        Random rnd = new Random();
+
         public void AppInput()
         {
-            Console.WriteLine("Unesite broj tredova koji ucestvuju u pogadjanju:");
-            IntInputForNumberOfParticipants();
+            Console.WriteLine("Unesite broj ucesnika koji ucestvuju u pogadjanju:");
+            numberOfParticipants = IntInputForNumberOfParticipants();
             Console.WriteLine("Unesite broj koji treba da se pogodi:");
-            IntInputForWantedNumber();
+            wantedNumber = IntInputForWantedNumber();
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Unesli ste {0} ucesnika u pogadjanju",numberOfParticipants);
+            Console.WriteLine("Broj koji treba da se pogodi je {0}",wantedNumber);
         }
 
         int IntInputForWantedNumber()
@@ -40,6 +50,29 @@ namespace DAN_XXXV_Dejan_Prodanovic
                     Console.WriteLine("Nevalidan unos.Unesite pozitivan ceo broj");
             } while (!succes);
             return inputValue;
+        }
+        //public void ThreadGeneratorMethod()
+        //{
+        //    for (int i = 0; i < numberOfParticipants; i++)
+        //    {
+        //        Thread t = new Thread();
+        //    }
+        //}
+
+        public void GuessWantedNumber()
+        {
+            int generatedNumber = rnd.Next(1,101);
+            //while (generatedNumber != wantedNumber)
+            //{
+            //    Thread.Sleep(100);
+            //}
+
+            if (generatedNumber == wantedNumber)
+            {
+                Console.WriteLine("pogodili ste  broj");
+            }  
+          
+            
         }
     }
 }
--- DAN_XXXV_Dejan_Prodanovic/DAN_XXXV_Dejan_Prodanovic/Program.cs
+++ DAN_XXXV_Dejan_Prodanovic/DAN_XXXV_Dejan_Prodanovic/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAN_XXXV_Dejan_Prodanovic
@@ -12,7 +13,15 @@ namespace DAN_XXXV_Dejan_Prodanovic
         static void Main(string[] args)
         {
             MyThreads mt = new MyThreads();
-            mt.AppInput();
+            Thread t1 = new Thread(mt.AppInput);
+            t1.Start();
+           
+            t1.Join();
+            for (int i = 0; i < 100; i++)
+            {
+                mt.GuessWantedNumber();
+            }
+            //mt.AppInput();
             Console.ReadLine();
         }
     }
